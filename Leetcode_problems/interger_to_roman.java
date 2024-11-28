73e6a43eba69da6152dc8aa5fed46270820866c3
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-11-28 23:29:05 +0530

    minor correction faster code

--- Leetcode_problems/interger_to_roman.java
+++ Leetcode_problems/interger_to_roman.java
@@ -4,14 +4,14 @@ class integer_to_roman {
     public static String intToRoman(int num) {
          int arr[]={1000 ,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,1};
       String str[]={"M" ,"CM","D", "CD", "C","XC","L","XL","X","IX","V","IV","I"};
-      String res="";
+      StringBuilder res=new StringBuilder();
       for(int i=0;i<arr.length;i++){
         while(num>=arr[i]){
-            res+=str[i];
+            res.append(str[i]);
             num-=arr[i];
         }
       }
-       return res;
+       return res.toString();
     }
     public static void main(String[] args) {
         int num=5995;
