04829d209356757c6a1ae208cce2553abe3ff56b
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-11-30 00:20:01 +0530

    Zigzag_Conversion problems

---
+++ Leetcode_problems/Zigzag_Conversion.java
@@ -0,0 +1,39 @@
+package Leetcode_problems;
+
+public class Zigzag_Conversion {
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+        StringBuilder[] sb = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++)
+            sb[i] = new StringBuilder();
+        int currRow = 0;
+        boolean isGoingDown = true;
+        for (char c : s.toCharArray()) {
+            sb[currRow] = sb[currRow].append(c);
+
+            if (currRow == 0) {
+                isGoingDown = true;
+            } else if (currRow == numRows - 1) {
+                isGoingDown = false;
+            }
+
+            currRow += isGoingDown ? 1 : -1;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : sb)
+            res.append(row);
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Zigzag_Conversion obj = new Zigzag_Conversion();
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(obj.convert(s, numRows));
+    }
+}
