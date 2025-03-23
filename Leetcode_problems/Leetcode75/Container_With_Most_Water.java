a0c73ca89d7e7d74e6227c4b4e1740a7d673436f
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-03-24 03:17:00 +0530

    more leetcode problems

---
+++ Leetcode_problems/Leetcode75/Container_With_Most_Water.java
@@ -0,0 +1,24 @@
+
+//Answer to the  question https://leetcode.com/problems/container-with-most-water/
+class Container_With_Most_Water {
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(maxArea(height));
+    }
+}
