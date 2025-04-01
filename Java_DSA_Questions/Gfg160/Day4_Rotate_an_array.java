a570b613ad510f7b083092d8706d9a927c6ca58d
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-04-01 11:24:24 +0530

    new problem added

--- Java_DSA_Questions/Gfg160/Day4_Rotate_an_array.java
+++ Java_DSA_Questions/Gfg160/Day4_Rotate_an_array.java
@@ -7,7 +7,10 @@ public class Day4_Rotate_an_array {
         reverse(nums,0,n-1);//reverse the whole array
         reverse(nums,0,k-1);//reverse first k elements
         reverse(nums,k,n-1);//reverse the rest of the elements
-        
+        //if we want to rotate the array in anti clock wise direction , we can do the same process but in reverse order.
+        //reverse(nums,0,k-1);
+        //reverse(nums,k,n-1);
+        //reverse(nums,0,n-1);
     }
     private void reverse( int arr[], int start, int end){
         while(start<end){
@@ -20,11 +23,11 @@ public class Day4_Rotate_an_array {
     }
     public static void main(String[] args) {
         Day4_Rotate_an_array obj = new Day4_Rotate_an_array();
-        int[] arr = {1, 2, 3, 4, 5, 6, 7}; // Example input
+        int[] arr = {1, 2, 3, 4, 5}; // Example input
         int k = 3; // Number of positions to rotate
         obj.rotate(arr, k);
         for (int i : arr) {
-            System.out.print(i + " "); // Output: 5 6 7 1 2 3 4
+            System.out.print(i + " "); // Output: 4 5 1 2 3
         }
     }
 }
---
+++ Java_DSA_Questions/Gfg160/Day5_Next_permutation.java
@@ -0,0 +1,48 @@
+package Java_DSA_Questions.Gfg160;
+public class Day5_Next_permutation {
+    public void reverse(int []arr, int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    void nextPermutation(int[] arr) {
+        // code here
+        int n=arr.length;
+         int index=-1;
+         for(int i=n-2;i>=0;i--){
+             if(arr[i]<arr[i+1]){
+                 index=i;//finding the index where the break point appears
+                 break;
+             }
+         }
+         if(index==-1){//if the whole array is already in the descending order then just return the next permutaion i.e. first permutation
+             reverse(arr,0,n-1);
+         }
+         else{
+         for(int i=n-1;i>=index;i--){//finding the least least larger number than the arr[index] and swaping it with arr[index]
+             if(arr[i]>arr[index]){
+                int temp=arr[i];
+                arr[i]=arr[index];
+                arr[index]=temp;
+                break;
+             }
+         }
+         
+         reverse(arr,index+1,n-1);//to get the next least larger number just reverse the subarray which is in descending order.
+         }
+        
+    }
+    public static void main(String[] args) {
+        Day5_Next_permutation obj = new Day5_Next_permutation();
+        int[] arr = {1, 2, 3, 6, 5, 4}; // Example input
+        obj.nextPermutation(arr);
+        for (int i : arr) {
+            System.out.print(i + " "); // Output: 1 2 4 3 5 6
+        }
+        System.out.println(); // For better readability in output
+    }
+}
