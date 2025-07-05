83c1679ef30c4bc527ac75cb38b7be5f00ec0899
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-07-06 00:44:52 +0530

    more of stack problems

--- Java_Core_Concepts/Stacks/Duplicate_parenthesis.java
+++ Java_Core_Concepts/Stacks/Duplicate_parenthesis.java
@@ -1,7 +1,36 @@
 package Java_Core_Concepts.Stacks;
 
+import java.util.Stack;
+
 public class Duplicate_parenthesis {
+
+    public static boolean checkDuplicate(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // case for closing brackets
+            if (ch == ')') {
+                int count = 0;
+                while (stack.pop() != '(') {// pop elements until we find the matching opening bracket
+                    count++;
+                }
+                if (count < 1) {
+                    return true;
+                }
+            } else {
+                stack.push(ch);// push the character onto the stack if it is not a closing bracket
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-         
+        String expression = "((a+b)+(c+d))";
+        if (checkDuplicate(expression)) {
+            System.out.println("Duplicate parenthesis found");
+        } else {
+            System.out.println("No duplicate parenthesis found");
+        }
+
     }
 }
--- Java_Core_Concepts/Stacks/Valid_parenthesis.java
+++ Java_Core_Concepts/Stacks/Valid_parenthesis.java
@@ -8,14 +8,16 @@ public class Valid_parenthesis {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < str.length(); i++) {
             char x = str.charAt(i);
-            if (x == '(' || x == '{' || x == '[') {
+            if (x == '(' || x == '{' || x == '[') {// opening brackets
+                // push the opening brackets onto the stack
                 st.push(x);
             } else {
-                if (st.isEmpty()) {
+                if (st.isEmpty()) {// if stack is empty and we encounter a closing bracket
+                    // it means there is no matching opening bracket
                     return false;
                 }
                 if ((st.peek() == '(' && x == ')') || (st.peek() == '{' && x == '}')
-                        || (st.peek() == '[' && x == ']')) {
+                        || (st.peek() == '[' && x == ']')) {// if the top of the stack is a matching opening bracket
                     st.pop();
                 } else {
                     return false;
