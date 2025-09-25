d0166720a557eafe3816363daa68f1d322fb25cf
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-09-26 04:30:29 +0530

    few more dsa questions

---
+++ Java_DSA_Questions/Other_Problems/Divide_two_Integers.java
@@ -0,0 +1,30 @@
+package Java_DSA_Questions.Other_Problems;
+
+public class Divide_two_Integers {
+
+    public static int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean sign = (dividend < 0) ^ (divisor < 0);
+
+        long a = (long) (Math.abs(dividend));
+        long b = (long) (Math.abs(divisor));
+        int result = 0;
+
+        while (a >= b) {
+            long temp = b, multiple = 1;
+            while (a >= temp * 2) {
+                temp *= 2;
+                multiple *= 2;
+            }
+            a = a - temp;
+            result += multiple;
+        }
+        return sign ? -result : result;
+    }
+    public static void main(String[] args) {
+        int dividend = 10, divisor = 3;
+        System.out.println(divide(dividend, divisor));
+    }
+}
---
+++ Java_DSA_Questions/Other_Problems/Swap_Nodes_Pairs.java
@@ -0,0 +1,56 @@
+package Java_DSA_Questions.Other_Problems;
+
+public class Swap_Nodes_Pairs {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode dummy=new ListNode(0);
+        ListNode prev=dummy.next;
+        while(prev.next!=null  && prev.next.next!=null){
+            ListNode first=prev.next;
+            ListNode second=prev.next.next;
+
+            prev.next=second.next;
+            first.next=second.next;
+            second.next=first;
+
+            prev=first;
+        }
+        return dummy.next;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head=new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(3);
+        head.next.next.next=new ListNode(4);
+
+        ListNode result=swapPairs(head);
+        while(result!=null){
+            System.out.print(result.val+" ");
+            result=result.next;
+        }
+        
+    }
+
+}
